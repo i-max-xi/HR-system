e77e550b8284d456019cbfb85ddfc442bda50435
Max <appiahmax4u@gmail.com>
2024-03-30 14:16:32 +0000

    setup resource and connect to dfatabase

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { EmployeesModule } from './employees/employees.module';
 
 @Module({
-  imports: [EmployeesModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      username: 'root',
+      password: '',
+      database: 'hrSystem',
+      entities: [],
+      synchronize: true,
+    }),
+    EmployeesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/employees/entities/employee.entity.ts
+++ src/employees/entities/employee.entity.ts
@@ -1,1 +1,30 @@
-export class Employee {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+@Entity()
+export class Employee {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  avatar: string;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  position: string;
+
+  @Column()
+  department: string;
+
+  @Column({ type: 'date' })
+  startDate: Date;
+}
